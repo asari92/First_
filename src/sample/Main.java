959fece29c5be9c11454c928733b439153d085ec
asari92 <dulmaev.andrei@gmail.com>
2020-04-04 22:37:33 +0800

    with trim

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input line: ");
-        StringBuilder line = new StringBuilder(in.nextLine());
-        int start = 0;
-        for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) != ' ') {
-                line.replace(i, i+1, "" + Character.toUpperCase(line.charAt(i)));
-                start = i;
-                break;
+        StringBuilder line = new StringBuilder(in.nextLine().trim()); //добавляем строку без пробелов по краям
+
+        line.replace(0, 1, "" + Character.toUpperCase(line.charAt(0))); //переводим в верх. регистр 1 символ
+
+        for (int i = 1; i < line.length(); i++) {
+            if (line.charAt(i) == '.' && i != line.length() - 1) {
+                for (int j = i + 1;; j++) {
+                    if (line.charAt(j) != ' ') {
+                        line.replace(j, j + 1, "" + Character.toUpperCase(line.charAt(j)));
+                        i = j;
+                        break;
+                    }
+                }
             }
         }
-        for (int i = start; i < line.length(); i++) {
-            if (i < 2) {
-                continue;
-            } else if (line.charAt(i) != ' ' && (line.charAt(i-2) == '.' || line.charAt(i-1) == '.')) {
-                line.replace(i,i+1, "" + Character.toUpperCase(line.charAt(i)));
-            }
-        }
+
         System.out.println(line);
         in.close();
     }

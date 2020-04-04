44caea91710df8965478e5b53e27189a649aca43
asari92 <dulmaev.andrei@gmail.com>
2020-04-05 00:11:25 +0800

    split with join

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,12 +9,17 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Input line: ");
 
-        String[] sentences = in.nextLine().trim().split(".");
+        String[] sentences = in.nextLine().trim().split("[.!?]");
 
         StringJoiner joiner = new StringJoiner(".");
 
         for (String sentence: sentences) {
-            joiner.add(Character.toUpperCase(sentence.charAt(0)) + sentence.substring(1));
+            for (int i = 0; i < sentence.length()-1; i++) {
+                if (sentence.charAt(i) != ' ') {
+                    joiner.add(Character.toUpperCase(sentence.charAt(i)) + sentence.substring(i+1));
+                    break;
+                }
+            }
         }
 
         System.out.println(joiner.toString());

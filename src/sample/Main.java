52eda9f8ac066aa501d93e31ef7077f282cb75f9
asari92 <dulmaev.andrei@gmail.com>
2020-04-05 16:37:39 +0800

    Character.isLetter() and try-with-resources

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,23 +6,23 @@ import java.util.StringJoiner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input line: ");
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Input line: ");
 
-        String[] sentences = in.nextLine().trim().split("[.!?]");
+            String[] sentences = in.nextLine().trim().split("[.!?]");
 
-        StringJoiner joiner = new StringJoiner(".");
+            StringJoiner joiner = new StringJoiner(".");
 
-        for (String sentence: sentences) {
-            for (int i = 0; i < sentence.length()-1; i++) {
-                if (sentence.charAt(i) != ' ') {
-                    joiner.add(Character.toUpperCase(sentence.charAt(i)) + sentence.substring(i+1));
-                    break;
+            for (String sentence : sentences) {
+                for (int i = 0; i < sentence.length() - 1; i++) {
+                    if (Character.isLetter(sentence.charAt(i))) {
+                        joiner.add(Character.toUpperCase(sentence.charAt(i)) + sentence.substring(i + 1));
+                        break;
+                    }
                 }
             }
-        }
 
-        System.out.println(joiner.toString());
-        in.close();
+            System.out.println(joiner.toString());
+        }
     }
 }
